9cdd9d6c9d7bd2e9e548bc6dd6032297cb8b1abe
panmin <panminzzu@126.com>
2018-03-02 18:36:47 +0800

    textAlign（'auto', 'left', 'right', 'center', 'justify'）：文本对齐方式

--- app/pages/homePage.js
+++ app/pages/homePage.js
@@ -5,6 +5,7 @@ import {
     FlatList,
     StyleSheet,
     Text,
+    ScrollView,
 } from 'react-native'
 import Toolbar from '../components/Toolbar'
 import Banner from '../components/banner';
@@ -31,7 +32,7 @@ export default class homePage extends Component{
 
     render(){
         return (
-            <View style={{flex:1,flexDirection:'column'}}>
+            <View style={{flex:1,flexDirection:'column',backgroundColor:'white'}}>
                 <View
                     style={{
                         justifyContent:'center',
@@ -49,22 +50,54 @@ export default class homePage extends Component{
                          centerImage={require('../resources/imgs/logo.png')}
                          rightButton={'bell-o'}
                 />
-                <FlatList
-                          horizontal={true}
-                          contentContainerStyle={styles.grid}
-                          data={this.state.IndexInfo.classify}
-                          keyExtractor={(item,index)=>index}
+                <ScrollView style={{flex:1}}>
+                    <Banner banners={this.state.IndexInfo.ad}/>
+                    <FlatList
+                              horizontal={true}
+                              contentContainerStyle={styles.grid}
+                              data={this.state.IndexInfo.classify}
+                              keyExtractor={(item,index)=>index}
 
-                          renderItem={(row,key)=> {
-                              return (
-                                  <View key={key} style={styles.itemLayout}>
-                                      <Image style={{width: 26, height: 26}} source={{uri:row.item.image}}/>
-                                      <Text>{row.item.name}</Text>
-                                  </View>
-                              )
-                          }}
-                />
-                <Banner banners={this.state.IndexInfo.ad}/>
+                              renderItem={(row,key)=> {
+                                  return (
+                                      <View key={key} style={styles.itemLayout}>
+                                          <Image style={{width: 16, height: 20,resizeMode:'contain'}} source={{uri:row.item.image}}/>
+                                          <Text style={{fontSize:8}}>{row.item.name}</Text>
+                                      </View>
+                                  )
+                              }}
+                    />
+                    <FlatList data={this.state.IndexInfo.article}
+                              contentContainerStyle={styles.article}
+                              ItemSeparatorComponent={()=>{
+                                  return (<View style={{height:10}}/>)
+                              }}
+                              keyExtractor={(item,index)=>index}
+                              renderItem={(row,key)=>{
+                                  return (
+                                      <View key={key} style={{width:'100%'}}>
+                                        <Image style={{width:'100%',height:200,resizeMode:'cover'}} source={{uri:row.item.thumb}}/>
+                                      </View>
+                                  )
+                              }}
+                    />
+                    <FlatList data={this.state.IndexInfo.favourite_goods}
+                              keyExtractor={(item,index)=>index}
+                              ItemSeparatorComponent={()=>{
+                                  return (<View style={{height:10}}/>)
+                              }}
+                              renderItem={(row,key)=>{
+                                  return (
+                                      <View key={key} style={{flexDirection:'row',width:'100%'}}>
+                                          <Image style={{flex:1,width:'50%',height:200,resizeMode:'cover'}} source={{uri:row.item.original_img}}/>
+                                          <View style={{width:'50%'}}>
+                                              <Text style={{textAlign:'center'}}>{row.item.goods_name}</Text>
+                                          </View>
+                                      </View>
+                                  )
+                              }}
+                    />
+                </ScrollView>
             </View>
         );
     }
@@ -81,5 +114,8 @@ const styles = StyleSheet.create({
     itemLayout:{
         alignItems:'center',
         justifyContent:'center',
+    },
+    article:{
+
     }
 });
